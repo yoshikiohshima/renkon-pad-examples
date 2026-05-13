96013e4dc9ebcb053365201e998fc9c20df47e3b
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2026-05-13 09:49:49 -0700

    update artwork.renkon

--- pad-demo-scripts/blowup.js
+++ pad-demo-scripts/blowup.js
@@ -4,7 +4,7 @@ const delayedTrigger = Events.once(Events.delay(decls, 250));
 ((decls) => {
   const code = new Map(decls.map((decl, id) => [`${id}`, decl]));
   const windows = decls.map((decl, id) => `${id}`);
-  const positions = new Map(decls.map((decl, id) => [`${id}`, {id: `${id}`, x: id * 5, y: id * 5, width: 300, height: 200}]));
+  const positions = new Map(decls.map((decl, id) => [`${id}`, {id: `${id}`, x: id * -5, y: id * -5, width: 300, height: 200}]));
   const titles = new Map(decls.map((decl, id) => {
     const state = new Renkon.constructor(0);
     state.setupProgram([decl]);
